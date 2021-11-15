e117f8def8fb5f0f7ed0f70069927e8cd35a05a4
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-11-15 17:54:55 +0200

    add catgories page but not finish and ad page our clients

--- js/main.js
+++ js/main.js
@@ -45,3 +45,24 @@ $('#line').css({
     },
   });
 
+
+
+  // grid and list for page categories
+  
+  var galleryWidgetElements = document.querySelectorAll('.gallery-widget');
+
+for (var i = 0, controlViewElement; i < galleryWidgetElements.length; i++)
+{
+	controlViewElement = galleryWidgetElements[i].querySelector('.control-view');
+	controlViewElement.galleryWidgetElement = galleryWidgetElements[i];
+	
+	controlViewElement.addEventListener('click', function ()
+	{
+		toggleView(this.galleryWidgetElement);
+	});
+}
+
+function toggleView(galleryWidgetElement)
+{
+	galleryWidgetElement.classList.toggle('list-view');
+}
